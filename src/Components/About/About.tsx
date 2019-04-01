66ad65a28bb41c693510d92d834f189115395f3a
Peter Taggart <staggarts@gmail.com>
2019-04-01 13:41:02 -0600

    cleaned up home page, added about me text

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BodyContainer, HeaderContainer } from '../Container';
+import './About.css';
 
 class About extends React.Component {
   render() {
@@ -7,7 +8,22 @@ class About extends React.Component {
       <React.Fragment>
         <HeaderContainer title='About Me' />
         <BodyContainer>
-          <p>I'm a 23 year old software engineer living in Denver.</p>
+          <p className='about_para'>
+            I'm a 23 year old software engineer living in Denver. I've worked in
+            many languages, with experience in C, C++, Javascript, Typescript,
+            Python, and Java. I've also worked with several different
+            frameworks, including nginx, puma, electron, React, FLTK, and
+            probably more. My current main interest is in blockchain
+            development, and I've done a couple of projects on top of the
+            blockchain, but none that involve working on the chain itself. I'm
+            currently working at Animatra Inc. on{' '}
+            <a href='https://lumiate.co'>Lumiate</a>, a Mindfulness app
+            utilizing a patented color overlay effect. I built most of the app
+            myself, using an Electron/React setup, with redux, firebase auth,
+            api usage, and more. I'm not currently interested in a new job, but
+            if you've got an interesting blockchain project or idea, I might
+            help out a bit.
+          </p>
         </BodyContainer>
       </React.Fragment>
     );
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BodyContainer, HeaderContainer } from '../Container';
+import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <HeaderContainer title='Peter James Taggart' />
         <BodyContainer>
-          <p>Words words words...</p>
+          <h3 className='home_header'>
+            <Link to='/portfolio' className='home_link'>
+              Portfolio
+            </Link>
+          </h3>
+
+          <p className='home_link_description'>
+            A sampling of my programming projects.
+          </p>
+
+          <h3 className='home_header'>
+            <Link to='/feed' className='home_link'>
+              My Feed
+            </Link>
+          </h3>
+
+          <p className='home_link_description'>Recent Steem Posts</p>
+
+          <h3 className='home_header'>
+            <Link to='/about' className='home_link'>
+              About Me
+            </Link>
+          </h3>
+
+          <p className='home_link_description'>Some information about me.</p>
         </BodyContainer>
       </React.Fragment>
     );
